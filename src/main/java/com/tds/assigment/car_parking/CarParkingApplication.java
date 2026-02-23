934253330643b412dad7bcc849d8968cdea868ba
nowakpawel <kontakt.nowakpawel@gmail.com>
2026-02-23 13:42:01 +0100

    Project initialization

---
+++ src/main/java/com/tds/assigment/car_parking/CarParkingApplication.java
@@ -0,0 +1,13 @@
+package com.tds.assigment.car_parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tds/assigment/car_parking/CarParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tds.assigment.car_parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
